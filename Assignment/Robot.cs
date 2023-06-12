eaa4df26a73f7473130b233994af71b6d38ab81c
arshpreet44 <134253496+arshpreet44@users.noreply.github.com>
2023-06-12 00:20:30 +0000

    update changes

--- Assignment/Robot.cs
+++ Assignment/Robot.cs
@@ -1,74 +1,65 @@
-﻿// Change to 'using Assignment.InterfaceCommand' when you are ready to test your interface implementation
-// using Assignment.AbstractCommand;
+﻿using System;
 using Assignment.interfaceCommand;
 
-namespace Assignment;
-
-public class Robot
+namespace Assignment
 {
-    // These are properties, you can replace these with traditional getters/setters if you prefer.
-    public int NumCommands { get; }
-    public int X { get; set; }
-    public int Y { get; set; }
-    public bool IsPowered { get; set; }
-
-    private const int DefaultCommands = 6;
-    // An array is not the preferred data structure here.
-    // You will get bonus marks if you replace the array with the preferred data structure
-    // Hint: It is NOT a list either,
-    private readonly RobotCommand[] _commands;
-    private int _commandsLoaded = 0;
-
-    public override string ToString()
+    public class Robot
     {
-        return $"[{X} {Y} {IsPowered}]";
-    }
-    //summary
-    // Constructor named "Robot" with no parameters. It has an initilizer that calls another constructor.
-    //summary
-    // dConstructor
-    // <modifier>public</modifier>
-    //<name>Robot</name> 
-    //parameters
-    // You should not have to use any of the methods below here but you should
-    // provide XML documentation for the argumented constructor, the Run method and one
-    // of the LoadCommand methods.
-    public Robot() : this(DefaultCommands) { }
+        // These are properties, you can replace these with traditional getters/setters if you prefer.
+        public int NumCommands { get; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public bool IsPowered { get; set; }
 
-    /// <summary>
-    ///  initializes the robot with the capacity to store a user specified
-    /// number of commands
-    /// </summary>
-    /// <param name="numCommands">The maximum number of commands the robot can store</param>
-    public Robot(int numCommands)
-    {
-        _commands = new RobotCommand[numCommands];
-        NumCommands = numCommands;
-    }
+        private const int DefaultCommands = 6;
+        // An array is not the preferred data structure here.
+        // You will get bonus marks if you replace the array with the preferred data structure
+        // Hint: It is NOT a list either,
+        private readonly RobotCommand[] _commands;
+        private int _commandsLoaded = 0;
 
-    /// <summary>
-    ///
-    /// </summary>
-    /// <throws> </throws>
-    public void Run()
-    {
-        for (var i = 0; i < _commandsLoaded; ++i)
+        public override string ToString()
         {
-            _command[i].Run(this);
-            Console.WriteLine(this);
+            return $"[{X} {Y} {IsPowered}]";
         }
-    }
 
-    /// <summary>
-    ///
-    /// </summary>
-    /// <param name="command"></param>
-    /// <returns></returns>
-    public bool LoadCommand(RobotCommand command)
-    {
-        if (_commandsLoaded >= NumCommands)
-            return false;
-        _commands[_commandsLoaded++] = command;
-        return true;
+        // Constructor named "Robot" with no parameters. It has an initializer that calls another constructor.
+        public Robot() : this(DefaultCommands) { }
+
+        /// <summary>
+        /// Initializes the robot with the capacity to store a user specified
+        /// number of commands
+        /// </summary>
+        /// <param name="numCommands">The maximum number of commands the robot can store</param>
+        public Robot(int numCommands)
+        {
+            _commands = new RobotCommand[numCommands];
+            NumCommands = numCommands;
+        }
+
+        /// <summary>
+        /// Runs the loaded commands on the robot
+        /// </summary>
+        public void Run()
+        {
+            for (var i = 0; i < _commandsLoaded; ++i)
+            {
+                _commands[i].Run(this);
+                Console.WriteLine(this);
+            }
+        }
+
+        /// <summary>
+        /// Loads a command into the robot's command list
+        /// </summary>
+        /// <param name="command">The command to load</param>
+        /// <returns>True if the command was successfully loaded, false otherwise</returns>
+        public bool LoadCommand(RobotCommand command)
+        {
+            if (_commandsLoaded >= NumCommands)
+                return false;
+            _commands[_commandsLoaded++] = command;
+            return true;
+        }
     }
 }
